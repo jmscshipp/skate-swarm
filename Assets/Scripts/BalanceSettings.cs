8b1ad097a50bfcbe1668757f8e49c55384648b6c
James Shipp <jmscshipp202@gmail.com>
2025-04-22 17:32:49 -0500

    working-ish version of character controller. switching to 3d

---
+++ Assets/Scripts/BalanceSettings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BalanceSettings
+{
+    // how long it takes for a push to peter out to 0f
+    public static float pushTime = 2f;
+    // how fast player travels at beginning of a push
+    public static float movementSpeed = 10f;
+}
