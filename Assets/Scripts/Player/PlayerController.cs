99e8d4155acc18c8bfc13196dd1b497fa15f7636
James Shipp <jmscshipp202@gmail.com>
2025-04-22 19:18:55 -0500

    progress on 3d version of character controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,10 +10,19 @@ public class PlayerController : MonoBehaviour
     private float movementSpeed;
     private PushQueue pushQueue;
 
+    private Rigidbody rb;
+
+    // amount the player is raised about the ground
+    private float playerDistanceFromGround = 0.132f;
+    // used with ground detection ray cast
+    [SerializeField]
+    private LayerMask groundMask;
+
     // Start is called before the first frame update
     void Start()
     {
         movementSpeed = BalanceSettings.movementSpeed;
+        rb = GetComponent<Rigidbody>();
         pushQueue = new PushQueue();
         pushQueue.AddPush();
 
@@ -21,19 +31,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveDir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
-        
-        transform.Translate(moveDir * Time.deltaTime * movementSpeed * pushDecayCurve.Evaluate(pushQueue.GetPushProgress()));
-
-        Debug.Log("push progress: " + pushQueue.GetPushProgress());
-        transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
-
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            pushQueue.AddPush();
-        }
-
-        pushQueue.Update(Time.deltaTime);
+        //Vector3 moveDir = (Camera.main.WorldToScreenPoint(transform.position) - Input.mousePosition).normalized;
+        //rb.velocity = moveDir * movementSpeed;
+        //Debug.DrawRay(transform.position, moveDir * 5f, Color.green);
+        //transform.Translate(moveDir * Time.deltaTime * movementSpeed * pushDecayCurve.Evaluate(pushQueue.GetPushProgress()));
+        //
+        //Debug.Log("push progress: " + pushQueue.GetPushProgress());
+        //transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
+        //
+        //
+        //if (Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //    pushQueue.AddPush();
+        //}
+        //
+        //pushQueue.Update(Time.deltaTime);
     }
+    private void FixedUpdate()
+    {
+        Vector3 moveDir = (Camera.main.WorldToScreenPoint(transform.position) - Input.mousePosition).normalized;
+        moveDir = new Vector3 (moveDir.x, 0f, moveDir.y);
+        Debug.DrawRay(transform.position, moveDir * 5f, Color.green);
+        rb.velocity = moveDir * Time.fixedDeltaTime * 100f;
+
+        //RaycastHit hit;
+        //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, groundMask))
+        //{
+        //    Debug.Log("hit " + hit.transform.gameObject.name);
+        //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.white);
+        //    transform.position = new Vector3(transform.position.x, hit.point.y + playerDistanceFromGround, transform.position.z);
+        //}
+        //else
+        //{
+        //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 1000, Color.red);
+        //}
+    }
+
 }
