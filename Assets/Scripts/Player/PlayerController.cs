c3a3c05ed4b66093df93f05c87d8ff4cd9e40c53
James Shipp <jmscshipp202@gmail.com>
2025-04-22 22:46:19 -0500

    cursor rotation working

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
 
+    [SerializeField]
+    private Transform directionalArrowUI;
+    private Vector3 screenCenter;
+
     // amount the player is raised about the ground
     private float playerDistanceFromGround = 0.132f;
     // used with ground detection ray cast
@@ -21,6 +26,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        UnityEngine.Cursor.visible = false;
+        screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
         movementSpeed = BalanceSettings.movementSpeed;
         rb = GetComponent<Rigidbody>();
         pushQueue = new PushQueue();
@@ -49,10 +56,16 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Vector3 moveDir = (Camera.main.WorldToScreenPoint(transform.position) - Input.mousePosition).normalized;
+        Vector3 moveDir = (screenCenter - Input.mousePosition).normalized;
+        Debug.Log(Input.mousePosition);
         moveDir = new Vector3 (moveDir.x, 0f, moveDir.y);
         Debug.DrawRay(transform.position, moveDir * 5f, Color.green);
         rb.velocity = moveDir * Time.fixedDeltaTime * 100f;
+
+        // rotate arrow UI in direction player is going
+        directionalArrowUI.transform.localRotation = Quaternion.Euler(
+            new Vector3(0f, 0f, 180f - (Mathf.Atan2(screenCenter.x - Input.mousePosition.x, 
+            screenCenter.y - Input.mousePosition.y) * Mathf.Rad2Deg)));
 
         //RaycastHit hit;
         //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, groundMask))
